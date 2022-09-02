e5c5a6321fa64e38fe56673e68e6a1f3d54f8448
KokiHara <111763782+KokiHK@users.noreply.github.com>
2022-09-02 11:38:07 +0900

    20220902

---
+++ src/FizzBuzz.java
@@ -0,0 +1,15 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// 1から100までの数値をループで処理する
+		for(int number =1; number<100; number++ );{
+			System.out.println();
+		}
+	
+		
+			//条件式　３で割り切れる数値の場合「Fizz」と表示する
+
+	}
+
+}
