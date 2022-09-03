17c865ac36dc420e5c60b34e9f291cd661e58df5
KokiHara <111763782+KokiHK@users.noreply.github.com>
2022-09-03 09:07:24 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,11 +3,23 @@ public class FizzBuzz {
 
 	public static void main(String[] args) {
 		// 1から100までの数値をループで処理する
-		for (int n=1; n<100; n++) {
+		for (int n=1; n<101; n++) {
+		//3かつ5の倍数の場合「FizzBizz」
+		if(n % 3==0 && n %5 ==0) {
+			System.out.println("FizzBIzz");
+		}
 		//条件式　３で割り切れる数値の場合「Fizz」と表示する
-		if( n % 3 == 0)
+		else if( n % 3 == 0) {
 			System.out.println("Fizz");
 		}
+		//条件式　5で割り切れる数値の場合「Buzz」とする
+		else if( n % 5 == 0) {
+			System.out.println("Buzz");
+		}
+		else 
+			System.out.println(n);
+		}
+	
 	}
 }
 
